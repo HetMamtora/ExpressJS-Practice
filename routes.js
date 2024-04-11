e90ba833e4473c325428fa51ef5657f547d3d09c
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-04-11 15:29:54 +0530

    updated

--- routes.js
+++ routes.js
@@ -17,9 +17,22 @@ app.get('/files',(req,res) => {
 })
 
 //JSON route
-app.get('/files',(req,res) => {
-    res.sendFile(path.join(__dirname,'index.html'));
+app.get('/jsonres',(req,res) => {
+    res.json([{
+        firstName: 'Thomas',
+        lastName: 'Shelby'
+    },{
+        firstName: 'Arthur',
+        lastName: 'Shelby'
+    }])
 })
+
+
+//non existing routes
+app.all('*',(req,res) => { //* means all the non existing pafes
+    res.send('Not Found');
+})
+
 
 app.listen(3000, () => {
     console.log("Server is running on Port:3000");
