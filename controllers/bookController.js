2d6bde8906c93d5ab9bccf60ba78c3b5bfbbb9cb
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-04-13 22:30:32 +0530

    updated

---
+++ controllers/bookController.js
@@ -0,0 +1,52 @@
+let books = [
+    {id:1, title:'Book-1', author:'Author-1'},
+    {id:2, title:'Book-2', author:'Author-2'}
+]
+
+//GET Method
+const getBooks = (req,res) => {
+    res.json(books);
+}
+
+//POST Method
+const createBooks = (req,res) => {
+    console.log(req.body);
+
+    const newBook = req.body;
+    newBook.id = books.length+1;
+    books.push(newBook);
+    res.status(201).json(books);    
+}
+
+//PUT Method - works on data updation
+const updateBooks = (req,res) => {
+    const id = parseInt(req.params.id);
+    const updatedBook = req.body;
+
+    const index = books.findIndex(book => book.id === id);
+
+    if(index !== -1){
+        books[index] = {...books[index], ...updatedBook};
+        res.json(books[index]);
+    }
+    else{
+        res.status(404).json({error:'Book not Found'});
+    }
+}
+
+//DELETE Method - to delete particular data
+const deleteBooks = (req,res) => {
+    const id = parseInt(req.params.id);
+    const index = books.findIndex(book => book.id === id);
+
+    if(index !== -1){
+        const deletedBook = books[index];
+        books.splice(index,1);
+        res.json(deletedBook);
+    }
+    else{
+        res.status(404).json({error:'Book not Found'});
+    }
+}
+
+module.exports = {getBooks, createBooks, updateBooks, deleteBooks};
