6b11db17047fc7e781f1950c9321a6c126642580
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-04-13 22:08:56 +0530

    Create other-methods.js

---
+++ other-methods.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+let books = [
+    {id:1, title:'Book-1', author:'Author-1'},
+    {id:2, title:'Book-2', author:'Author-2'}
+]
+
+//GET Method
+app.get('/books',(req,res) => {
+    res.json(books);
+})
+
+//POST Method
+app.post('/books',(req,res) => {
+    console.log(req.body);
+
+    const newBook = req.body;
+    newBook.id = books.length+1;
+    books.push(newBook);
+    res.status(201).json(books);    
+})
+
+//PUT Method - works on data updation
+app.put('/books/:id',(req,res) => {
+    const id = 
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on Port:3000");
+})
